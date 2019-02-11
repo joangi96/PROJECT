ca054e2627cad2d37dfd141f6efdce52c23a445c
Joan Giralt Ibañez <joangi96@gmail.com>
2019-02-11 15:20:32 +0100

    2nd mod PROG

--- HELLO.f90
+++ HELLO.f90
@@ -1,7 +1,8 @@
 program HE
 implicit none
-integer,parameter:: a = 1,b = 2
+integer,parameter:: a = 1,b = 2,c = 3
 
 print*,a
 print*,b, "1r cambio archivo"
+print*,c, "2nd cambio archivo"
 end program HE
